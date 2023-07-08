4dd4d0af29966d0370b16142f567860b0e734fc6
LUIZ VILARINHO NETO <luiz.vilarinho@portoseguro.com.br>
2023-07-08 14:53:30 -0300

    chore: .env ,nextConfig

--- next.config.js
+++ next.config.js
@@ -3,9 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  // assetPrefix: 'http://vilaweb.link/training',
-  basePath: '/training',
-  assetPrefix: '/training/'
+ 
 }
 
 module.exports = nextConfig
